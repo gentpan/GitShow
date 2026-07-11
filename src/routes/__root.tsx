5da9a04600a33464455b858cc5f8b61f91bbd387
gentpan <dev@gentpan.com>
2026-07-11 00:00:54 +0000

    style: 使用 GitHub markdown 样式修复 README 排版

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,14 @@
 import { HeadContent, Outlet, Scripts, createRootRoute } from '@tanstack/react-router'
 import { SiteLayout } from '@/components/SiteLayout'
 import appCss from '@/styles.css?url'
+import markdownReadmeCss from '@/markdown-readme.css?url'
 
 export const Route = createRootRoute({
   head: () => ({
     meta: [{ charSet: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { title: 'GitShow' }],
     links: [
       { rel: 'stylesheet', href: appCss },
+      { rel: 'stylesheet', href: markdownReadmeCss },
       { rel: 'icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Geist:wght@400;500;600;700&display=swap' },
       { rel: 'stylesheet', href: 'https://static.utterlog.com/libs/fontawesome/7.2.0/css/all.min.css' },
